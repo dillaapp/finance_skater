58f1a0f7be28e88d649b231a4a30efc4bb59606a
Amanuel <aman.nigussie@gmail.com>
2023-02-11 18:07:36 +0200

    Removed unused imports

--- financial_advisors/views.py
+++ financial_advisors/views.py
@@ -1,8 +1,6 @@
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+
 
 from .models import *
 
